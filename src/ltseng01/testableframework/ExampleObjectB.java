d16c2e9e98f6ba74d7c19ba637ab7af6e8963dc0
Larry Tseng <lt222888@gmail.com>
2018-11-04 13:17:45 -0500

    Add TestManager and NTC functionality, new Result enum

--- src/ltseng01/testableframework/ExampleObjectB.java
+++ src/ltseng01/testableframework/ExampleObjectB.java
@@ -1,14 +1,29 @@
 package ltseng01.testableframework;
 
-public class ExampleObjectB implements Testable {
+public class ExampleObjectB {
 
-    public ExampleObjectB() {
-        // TestManager.registerTest("name", this);
+    public ExampleObjectB(String name) {
+
+        TestManager.registerTest("ExampleObjectB-" + name, this::testProcedure);
+
+        TestManager.registerTest(name, () -> {
+
+            Result result = Result.PASS;
+            String expected = "10";
+            String actual = "11";
+
+            return new TestResult().setTestResults(name, result, expected, actual);
+        });
+
     }
 
-    @Override
     public TestResult testProcedure() {
-        return null;
+        Result result = Result.FAIL;
+        String expected = "15";
+        String actual = "14";
+
+        return new TestResult().setTestResults("Hi", result, expected, actual);
     }
+
 
 }
--- src/ltseng01/testableframework/NetworkTablesCommunication.java
+++ src/ltseng01/testableframework/NetworkTablesCommunication.java
@@ -3,34 +3,48 @@ package ltseng01.testableframework;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
+import java.util.HashMap;
+
 public class NetworkTablesCommunication {
+
+    private static NetworkTableInstance inst;
+    private static NetworkTable mainTable;
+    private static NetworkTable statusTable;
+    private static NetworkTable finishedTable;
 
     public NetworkTablesCommunication() {
 
         // Network Tables Setup
-        NetworkTableInstance inst = NetworkTableInstance.getDefault();
-        NetworkTable mainTable = inst.getTable("RobotTestingFramework");
-        NetworkTable statusTable = mainTable.getSubTable("status");
-        NetworkTable finishedTable = mainTable.getSubTable("finishedTests");
+
+        inst = NetworkTableInstance.getDefault();
+        mainTable = inst.getTable("RobotTestingFramework");
+        statusTable = mainTable.getSubTable("status");
+        finishedTable = mainTable.getSubTable("finishedTests");
 
         inst.startClient("localhost");
 
-        //
         mainTable.getEntry("availableTests").setStringArray(new String[]{"test1"});
         statusTable.getEntry("running").setBoolean(false);
-        finishedTable.getEntry("test1").setStringArray(new String[]{"Test1", "Pass"});
+        finishedTable.getEntry("test1").setStringArray(new String[]{"Test1", "Result"});
 
     }
 
 
     // listen to change in 'RobotTestingFramework/status/running'
+    // start or stop TestProcedures
 
     public static void publishTestInfo(String[] tests) {
 
     }
 
-    public static void publishTestResult(TestResult result) {
+    public static String[] receiveTestsToRun() {
+        return null;
+    }
 
+    public static void publishTestResult(HashMap<String, TestResult> testResults) {
+        testResults.forEach((name, testResult) -> {
+            finishedTable.getEntry(name).setStringArray(testResult.getTestResults());
+        });
     }
 
     public static void transmitInstructions(String instructions) {
